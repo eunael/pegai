38231bfc834221125e67cc1c45d0f056c210a35e
Natanael A. de Morais <natanaelallves@gmail.com>
2025-01-12 20:22:11 -0300

    Initial setup

---
+++ src/Service/S3ClientService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+
+use Aws\Credentials\Credentials;
+use Aws\S3\S3Client;
+
+class S3ClientService
+{
+    private S3Client $s3Client;
+
+    public function __construct(
+        string $bucketName,
+        string $accessKeyId,
+        string $secretAccessKey,
+        string $endpoint
+    )
+    {
+        $credentials = new Credentials($accessKeyId, $secretAccessKey);
+
+        $this->s3Client = new S3Client([
+            'region' => 'auto',
+            'endpoint' => $endpoint,
+            'version' => 'latest',
+            'credentails' => $credentials
+        ]);
+    }
+
+    public function getClient(): S3Client
+    {
+        return $this->s3Client;
+    }
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\UploadFileTest;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class UploadFileTest extends KernelTestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
