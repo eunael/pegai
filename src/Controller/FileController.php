6eacb57f8ba109171b6a3bea8d4e5f6c5c63f358
Natanael A. de Morais <natanaelallves@gmail.com>
2025-01-12 23:46:38 -0300

    wip: download file by id

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -5,25 +5,26 @@ namespace App\Controller;
 use App\Entity\File;
 use App\Repository\FileRepository;
 use App\Service\S3ClientService;
-use Aws\Credentials\Credentials;
-use Aws\S3\S3Client;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
-use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class FileController extends AbstractController
 {
     private S3ClientService $s3ClientService;
     private FileRepository $fileRepository;
+    private HttpClientInterface $httpClient;
 
-    public function __construct(S3ClientService $s3ClientService, FileRepository $fileRepository)
+    public function __construct(S3ClientService $s3ClientService, FileRepository $fileRepository, HttpClientInterface $httpClient)
     {
         $this->s3ClientService = $s3ClientService;
 
         $this->fileRepository = $fileRepository;
+        
+        $this->httpClient = $httpClient;
     }
 
     #[Route('/file', name: 'app_index', methods: ['GET'])]
@@ -71,4 +72,41 @@ class FileController extends AbstractController
         ]);
     }
 
+    #[Route('/download/{file}', name: 'app_download', methods: ['GET'])]
+    public function download(File $file): Response
+    {
+        $fileKey = $file->getKey();
+
+        $s3 = $this->s3ClientService->getClient();
+
+        $cmd = $s3->getCommand('GetObject', [
+            'Bucket' => $this->getParameter('aws.bucket_name'),
+            'Key' => $fileKey
+        ]);
+
+        $request = $s3->createPresignedRequest($cmd, '+5 minutes');
+        
+        // retorna uma url de download, mas eu quero fazer um preview
+        $signedUrl = (string) $request->getUri();
+
+        #####
+        try {
+            $response = $this->httpClient->request('GET', $signedUrl);
+            // conteudo do arquivo, mas qnd é pdf retorna o binário
+            $content = $response->getContent();
+            $contentType = $response->getHeaders()['content-type'][0] ?? 'application/pdf';
+
+            // Retorne o conteúdo com o cabeçalho adequado para exibição
+            return new Response($content, Response::HTTP_OK, [
+                'Content-Type' => $contentType,
+                'Content-Disposition' => 'inline; filename="preview.pdf"',
+            ]);
+        } catch (\Exception $e) {
+            return new Response('Erro ao buscar o arquivo: ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $this->json([
+            'signedUrl' => $signedUrl
+        ]);
+    }
 }
